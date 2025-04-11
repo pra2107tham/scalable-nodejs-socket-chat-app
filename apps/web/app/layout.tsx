e3453e66cf27c3ede4675885bd72b4983679f2b6
pra2107tham <prathamshirbhate1909@gmail.com>
2025-04-12 01:54:39 +0530

    feat(socket): integrate socket.io client and provider in web app, update server configuration for CORS

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SockerProvider } from "../context/SocketProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,7 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <SockerProvider>
+          {children}
+        </SockerProvider>
       </body>
     </html>
   );
---
+++ apps/web/context/SocketProvider.tsx
@@ -0,0 +1,35 @@
+"use client";   
+import React, { useEffect } from "react";
+import io from "socket.io-client"; 
+
+interface SocketContextType {
+    children?: React.ReactNode;
+}
+interface ISocketContext {
+    sendMessage: (message: string) => any;
+}
+
+const SocketContext = React.createContext<ISocketContext | null>(null);
+
+export const SockerProvider: React.FC<SocketContextType> = ({ children }) => {
+    const sendMessage: ISocketContext["sendMessage"] = (message: string) => {
+        console.log("sendMessage", message);
+    }
+
+    useEffect(() => {
+        const socket = io("http://localhost:8000");
+        socket.on("event:message", (message: string) => {
+            console.log("message", message);
+        });
+
+        return () => {
+            socket.off("event:message");
+        }
+    }, []);
+
+    return(
+        <SocketContext.Provider value={{ sendMessage }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
